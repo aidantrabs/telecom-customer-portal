67f50c1c5c1bcd29d5399eaced9cd243e0e76d0f
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-04-16 20:07:58 -0400

    feat: add customer profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,3 +10,11 @@ class User(AbstractUser):
         ADMIN = 'admin', 'Admin'
 
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.CUSTOMER)
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='customer')
+    account_number = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.account_number
