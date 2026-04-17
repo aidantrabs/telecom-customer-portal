36dcb290b0060d806d1ba1544c1068bee9380c35
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-04-16 20:42:10 -0400

    feat: register models in django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Customer, User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
+    list_display = ('username', 'email', 'role', 'is_staff')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('account_number', 'user')
+    search_fields = ('account_number', 'user__username')
--- complaints/admin.py
+++ complaints/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Complaint
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'status', 'customer', 'assigned_agent', 'created_at')
+    list_filter = ('status', 'category')
+    search_fields = ('description', 'customer__account_number')
